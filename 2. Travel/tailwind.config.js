5189630f863ca249004bccfc27ac6fd2085e8719
Gus Alitt <idabagusalitbhadrayoga@gmail.com>
2024-01-22 10:45:10 +0700

    Menambahkan web travel

---
+++ 2. Travel/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inter: ["Inter"],
+      },
+      colors: {
+        ungu: "#9333ea",
+        pink: "#F85E9F",
+        // pink: "#9333ea",
+        orange: "#FF5722",
+        kuning: "#FACD49",
+      },
+    },
+  },
+  plugins: [],
+};
